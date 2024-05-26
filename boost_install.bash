2609a9f7218ebbd9cb2c6c531eb907ed493c7281
Alvin He <zhongjihe.alvin@gmail.com>
2024-05-26 14:12:18 -0700

    added boost install script

---
+++ boost_install.bash
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+BOOST_HEADER_DIR="./boost_1_85_0"
+
+WS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $WS_DIR
+set -ex 
+
+cd ./cpp
+mkdir include
+mkdir libs
+
+cd $BOOST_HEADER_DIR
+
+echo "compiling boost with cobalt"
+sh ./bootstrap.sh
+./b2 link=static cxxstd=20 --with-cobalt
+
+# creating symlinks
+ln -s $(readlink -f "./stage/lib") "$(readlink -f "../libs")/boost"
+ln -s $(readlink -f "./boost") "$(readlink -f "../include")/boost"
