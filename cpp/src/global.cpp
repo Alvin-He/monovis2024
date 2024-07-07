67cd9c36d1f65b8b7aaa55d4412c0f46fcfd5ed7
Alvin He <zhongjihe.alvin@gmail.com>
2024-07-07 15:47:54 -0700

    added the local developer overwrite for global.cpp

--- cpp/src/global.cpp
+++ cpp/src/global.cpp
@@ -2,7 +2,11 @@
 #pragma once
 
 
-#define DEBUG true
+// #define DEBUG true
 // #define GUI true
+
+#if __has_include("dev_overwrite_global.cpp")
+    #include "dev_overwrite_global.cpp"
+#endif
 
 constexpr double APRILTAG_BLOCK_SIZE_cm = 2.0;  
