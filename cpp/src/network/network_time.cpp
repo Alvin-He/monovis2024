c0a35d378e44de2ecd17cfd055dc69f1237b1419
Alvin He <zhongjihe.alvin@gmail.com>
2024-08-31 11:53:12 -0700

    update StartPeriodicLatencyUpdate to use wpilib's event listener

--- cpp/src/network/network_time.cpp
+++ cpp/src/network/network_time.cpp
@@ -7,29 +7,23 @@
 #include <boost/asio/steady_timer.hpp>
 
 namespace NetworkTime {
-    int64_t serverTimeOffset = nt::NetworkTableInstance::GetDefault().GetServerTimeOffset().value_or(0);
+    std::atomic<int64_t> serverTimeOffset_us = nt::NetworkTableInstance::GetDefault().GetServerTimeOffset().value_or(0);
 
     bool ___is_StartPeriodicLatencyUpdate_launched = false; 
     // Update loop to periodically update the local cache of the server time offset
-    cobalt::detached StartPeriodicLatencyUpdate(const std::chrono::milliseconds updateInterval = 1000ms) {
-        if (___is_StartPeriodicLatencyUpdate_launched) throw std::logic_error("StartPeriodicLatencyUpdate is called already."); 
+    void StartPeriodicLatencyUpdate() {
+        if (___is_StartPeriodicLatencyUpdate_launched) throw std::logic_error("StartPeriodicLatencyUpdate has already been started."); 
         ___is_StartPeriodicLatencyUpdate_launched = true;
 
-        boost::asio::steady_timer timer{co_await cobalt::this_coro::executor, updateInterval};
-        for(;;) {
-            auto offset = nt::NetworkTableInstance::GetDefault().GetServerTimeOffset();
-            if (offset.has_value()) {
-                NetworkTime::serverTimeOffset = offset.value(); 
-            }
-            co_await timer.async_wait(cobalt::use_op);
-            timer.expires_at(timer.expires_at() + updateInterval);
-        }
+        nt::NetworkTableInstance::GetDefault().AddTimeSyncListener(true, [](const nt::Event& e) {
+            NetworkTime::serverTimeOffset_us = e.GetTimeSyncEventData()->serverTimeOffset;
+        }); 
     };
 
     // returns the current server time
     // can be used in threads, serverTimeOffset is not procted by a lock, but it donesn't need it
     int64_t Now() {
-        return nt::Now() + NetworkTime::serverTimeOffset;
+        return nt::Now() + NetworkTime::serverTimeOffset_us;
     }
 
 }
