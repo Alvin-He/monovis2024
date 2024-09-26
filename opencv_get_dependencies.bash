770f32fa33aac2e408f958cc09b5abdf918ddebc
Alvin-He <shinypeach36@gmail.com>
2024-09-25 21:21:14 -0700

    updates opencv build scripts

--- opencv_get_dependencies.bash
+++ opencv_get_dependencies.bash
@@ -5,8 +5,12 @@ if [[ $UID -ne 0 ]]; then
     exit 1
 fi
 
+# default to download build tools also, unless given any argument to make this script download libraries only
+if [[ -z "$1" ]]; then
 sudo apt install wget tar -y
 sudo apt install build-essential cmake pkg-config unzip yasm git checkinstall -y
+fi
+
 sudo apt install libjpeg-dev libpng-dev libtiff-dev -y 
 sudo apt install libavcodec-dev libavformat-dev libswscale-dev -y 
 sudo apt install libxvidcore-dev x264 libx264-dev libfaac-dev libmp3lame-dev libtheora-dev -y
@@ -17,3 +21,5 @@ sudo apt-get install libgtk-3-dev -y
 sudo apt-get install libtbb-dev -y
 sudo apt-get install libatlas-base-dev gfortran -y 
 sudo apt-get install libeigen3-dev -y 
+
+sudo apt install python3 python3-pip python3-numpy -y
